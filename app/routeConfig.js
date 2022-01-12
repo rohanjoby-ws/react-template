1372e8c7b2c8febb48e4d698b18a2283976934ce
rohanjoby-ws <rohan.joby@wednesday.is>
2022-01-12 23:04:03 +0530

    feat: make ItunesContainer render in / route

--- app/routeConfig.js
+++ app/routeConfig.js
@@ -1,9 +1,9 @@
 import NotFound from '@containers/NotFoundPage/Loadable';
-import HomeContainer from '@containers/HomeContainer/Loadable';
+import ITunesContainer from '@containers/ITunesContainer/Loadable';
 import routeConstants from '@utils/routeConstants';
 export const routeConfig = {
-  repos: {
-    component: HomeContainer,
+  iTunes: {
+    component: ITunesContainer,
     ...routeConstants.repos
   },
   notFoundPage: {
