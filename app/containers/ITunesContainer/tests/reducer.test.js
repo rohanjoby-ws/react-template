fb0603267626832cf7aa0cad9c439067b5b9f5f9
rohanjoby-ws <rohan.joby@wednesday.is>
2022-01-16 15:13:36 +0530

    test: improve test coverage to 100%

--- app/containers/ITunesContainer/tests/reducer.test.js
+++ app/containers/ITunesContainer/tests/reducer.test.js
@@ -1,4 +1,3 @@
-// import produce from 'immer'
 import { iTunesContainerReducer, iTunesContainerTypes, initialState } from '../reducer';
 
 /* eslint-disable default-case, no-param-reassign */
@@ -12,13 +11,44 @@ describe('ITunesContainer reducer tests', () => {
     expect(iTunesContainerReducer(undefined, {})).toEqual(state);
   });
 
-  it('should return the update the state when an action of type DEFAULT is dispatched', () => {
-    const expectedResult = { ...state, somePayload: 'Mohammed Ali Chherawalla' };
+  it('should return the initial state when an action of type FETCH_SONGS is dispatched', () => {
+    const searchQuery = 'Infinity';
+    const expectedResult = { ...state, searchQuery };
     expect(
       iTunesContainerReducer(state, {
-        type: iTunesContainerTypes.DEFAULT_ACTION,
-        somePayload: 'Mohammed Ali Chherawalla'
+        type: iTunesContainerTypes.REQUEST_GET_I_TUNES_SONGS,
+        searchQuery
       })
     ).toEqual(expectedResult);
+  });
+
+  it('should ensure that the user data is present when FETCH_SONGS_SUCCESS is dispatched', () => {
+    const data = { totalCount: 1, items: [{ artistName: 'Jaymes Young' }] };
+    const expectedResult = { ...state, iTunesData: data };
+    expect(
+      iTunesContainerReducer(state, {
+        type: iTunesContainerTypes.SUCCESS_GET_I_TUNES_SONGS,
+        data
+      })
+    ).toEqual(expectedResult);
+  });
+
+  it('should ensure that the userErrorMessage has some data when FETCH_SONGS_FAILURE is dispatched', () => {
+    const error = 'something_went_wrong';
+    const expectedResult = { ...state, iTunesError: error };
+    expect(
+      iTunesContainerReducer(state, {
+        type: iTunesContainerTypes.FAILURE_GET_I_TUNES_SONGS,
+        error
+      })
+    ).toEqual(expectedResult);
+  });
+
+  it('should return the initial state when CLEAR_I_TUNES is dispatched', () => {
+    expect(
+      iTunesContainerReducer(state, {
+        type: iTunesContainerTypes.CLEAR_GET_I_TUNES_SONGS
+      })
+    ).toEqual(initialState);
   });
 });
--- app/containers/ITunesContainer/tests/saga.test.js
+++ app/containers/ITunesContainer/tests/saga.test.js
@@ -3,18 +3,48 @@
  */
 
 /* eslint-disable redux-saga/yield-effects */
-import { takeLatest } from 'redux-saga/effects';
-import iTunesContainerSaga, { defaultFunction } from '../saga';
+import { takeLatest, call, put } from 'redux-saga/effects';
+import { getSongs } from '@services/iTunesApi';
+import { apiResponseGenerator } from '@utils/testUtils';
+import iTunesContainerSaga, { getITunesSongs } from '../saga';
 import { iTunesContainerTypes } from '../reducer';
 
 describe('ITunesContainer saga tests', () => {
   const generator = iTunesContainerSaga();
-  const getITunesSongGenerator = defaultFunction();
+  const searchQuery = 'Infinity';
+  let getITunesSongGenerator = getITunesSongs({ searchQuery });
 
-  it('should start task to watch for DEFAULT_ACTION action', () => {
-    expect(generator.next().value).toEqual(takeLatest(iTunesContainerTypes.DEFAULT_ACTION, defaultFunction));
+  it('should start task to watch for REQUEST_GET_I_TUNES_SONGS action', () => {
+    expect(generator.next().value).toEqual(takeLatest(iTunesContainerTypes.REQUEST_GET_I_TUNES_SONGS, getITunesSongs));
   });
-  it('should return undefined', () => {
-    expect(getITunesSongGenerator.next().value).toEqual(undefined);
+
+  it('should ensure that the action FAILURE_GET_I_TUNES_SONGS is dispatched when the api call fails', () => {
+    const res = getITunesSongGenerator.next().value;
+    expect(res).toEqual(call(getSongs, searchQuery));
+    const errorResponse = {
+      errorMessage: 'There was an error while fetching song information.'
+    };
+    expect(getITunesSongGenerator.next(apiResponseGenerator(false, errorResponse)).value).toEqual(
+      put({
+        type: iTunesContainerTypes.FAILURE_GET_I_TUNES_SONGS,
+        error: errorResponse
+      })
+    );
+  });
+
+  it('should ensure that the action SUCCESS_GET_I_TUNES_SONGS is dispatched when the api call succeeds', () => {
+    getITunesSongGenerator = getITunesSongs({ searchQuery });
+    const res = getITunesSongGenerator.next().value;
+    expect(res).toEqual(call(getSongs, searchQuery));
+    const iTunesResponse = {
+      totalCount: 1,
+      items: [{ artistName: 'Jaymes Young' }]
+    };
+    expect(getITunesSongGenerator.next(apiResponseGenerator(true, iTunesResponse)).value).toEqual(
+      put({
+        type: iTunesContainerTypes.SUCCESS_GET_I_TUNES_SONGS,
+        data: iTunesResponse
+      })
+    );
   });
 });
