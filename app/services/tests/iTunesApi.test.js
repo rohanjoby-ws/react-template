8fa78a8ebdeea1b265021b801df2a6a6bb397060
rohanjoby-ws <rohan.joby@wednesday.is>
2022-01-20 19:41:23 +0530

    feat: add service for song lookUp route

--- app/services/tests/iTunesApi.test.js
+++ app/services/tests/iTunesApi.test.js
@@ -1,6 +1,6 @@
 import MockAdapter from 'axios-mock-adapter';
 import { getApiClient } from '@utils/apiUtils';
-import { getSongs } from '../iTunesApi';
+import { getSongs, getTrackDetails } from '../iTunesApi';
 
 describe('iTunesApi tests', () => {
   const searchQuery = 'Infinity';
@@ -8,12 +8,26 @@ describe('iTunesApi tests', () => {
     const mock = new MockAdapter(getApiClient().axiosInstance);
     const itunesData = [
       {
-        totalCount: 1,
+        resultCount: 1,
         items: [{ searchQuery }]
       }
     ];
     mock.onGet(`/search?term=${searchQuery}`).reply(200, itunesData);
     const res = await getSongs(searchQuery);
     expect(res.data).toEqual(itunesData);
+  });
+
+  const trackId = '284910350';
+  it('should make the api call to "/lookup?id="', async () => {
+    const mock = new MockAdapter(getApiClient().axiosInstance);
+    const trackData = [
+      {
+        resultCount: 1,
+        items: [{ trackId }]
+      }
+    ];
+    mock.onGet(`/lookup?id=${trackId}`).reply(200, trackData);
+    const res = await getTrackDetails(trackId);
+    expect(res.data).toEqual(trackData);
   });
 });
