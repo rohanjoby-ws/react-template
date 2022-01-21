578c2365a1f0704827c82979d744b93840ffbf48
rohanjoby-ws <rohan.joby@wednesday.is>
2022-01-21 20:37:30 +0530

    test: update test coverage

--- app/containers/ITunesContainer/tests/index.test.js
+++ app/containers/ITunesContainer/tests/index.test.js
@@ -139,16 +139,19 @@ describe('<ITunesContainer /> container tests', () => {
     await timeout(500);
     expect(baseElement.getElementsByClassName('ant-skeleton').length).toBe(1);
   });
+
   it('should test play/pause functionality', async () => {
     const iTunesData = { totalCount: 2, results: [{ artistName: 'Jaymes' }, { artistName: 'Young' }] };
     const { getAllByTestId } = renderProvider(
       <ITunesContainer iTunesData={iTunesData} dispatchITunesSongs={submitSpy} />
     );
     const players = getAllByTestId('itunes-card');
-    players.forEach((item) => {
-      fireEvent.play(item.querySelector('audio'));
+    const buttons = getAllByTestId('play-button');
+
+    buttons.forEach((item) => {
+      fireEvent.click(item);
     });
-    await timeout(500);
+
     expect(players[0].querySelector('audio')).toHaveProperty('paused', true);
   });
 });
