ca920fb1714ad97d1912708f40efe22ba28962b6
rohanjoby-ws <rohan.joby@wednesday.is>
2022-02-01 14:37:58 +0530

    update webpack configs, history

--- app/utils/history.js
+++ app/utils/history.js
@@ -1,4 +1,23 @@
 import { createBrowserHistory } from 'history';
-export const baseUrl = () => (process.env.NODE_ENV === 'production' ? '/react-template' : '/');
-const history = createBrowserHistory();
+import routeConstants from '@utils/routeConstants';
+
+const routes = Object.keys(routeConstants);
+const pathname = window.location.pathname;
+
+let bUrl = '';
+if (process.env.ENVIRONMENT_NAME === 'uat') {
+  routes.forEach((routeKey) => {
+    const route = routeConstants[routeKey].route;
+    if (pathname.includes(route)) {
+      if (pathname.substring(pathname.length - route.length, pathname.length) === route) {
+        bUrl = pathname.substring(0, pathname.length - route.length);
+      }
+      if (pathname.substring(pathname.length - route.length, pathname.length - 1) === `${route}/`) {
+        bUrl = pathname.substring(0, pathname.length - route.length - 1);
+      }
+    }
+  });
+}
+
+const history = createBrowserHistory({ basename: bUrl });
 export default history;
--- app/utils/tests/history.test.js
+++ app/utils/tests/history.test.js
@@ -1,21 +1,19 @@
-import { baseUrl } from '../history';
-
+import history from '../history';
 describe('Tests for baseUrl method in history', () => {
   const OLD_ENV = process.env;
   beforeEach(() => {
     jest.resetModules();
     process.env = { ...OLD_ENV };
   });
-
   afterAll(() => {
     process.env = OLD_ENV;
   });
-  it('should the path /react-template in production', () => {
+  it('should the path / in production', () => {
     process.env.NODE_ENV = 'production';
-    expect(baseUrl()).toEqual('/react-template');
+    expect(history.location.pathname).toEqual('/');
   });
 
-  it('should the path /react-template in development or test mode', () => {
-    expect(baseUrl()).toEqual('/');
+  it('should the path / in development', () => {
+    expect(history.location.pathname).toEqual('/');
   });
 });
