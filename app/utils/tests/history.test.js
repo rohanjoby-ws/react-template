3df3cf1c58782f738729b58b62a6b02a197f212f
rohanjoby-ws <rohan.joby@wednesday.is>
2022-02-01 21:07:51 +0530

    update history, tests

--- app/utils/tests/history.test.js
+++ app/utils/tests/history.test.js
@@ -1,43 +1,25 @@
 describe('Tests for baseUrl method in history', () => {
-  let baseUrl;
-  let route;
-  let pathname;
+  it('should not return the branch-name as part of the baseUrl when the ENVIRONMENT_NAME is not uat', () => {
+    const featName = `/feat/uat-on-pr`;
+    const location = window.location;
+    delete window.location;
+    window.location = { pathname: `${featName}/` };
+    process.env.ENVIRONMENT_NAME = 'dev';
+    const { getBaseUrl } = require('../history');
 
-  const OLD_ENV = process.env;
-  beforeEach(() => {
-    baseUrl = '';
-    process.env = { ...OLD_ENV };
+    expect(getBaseUrl()).not.toBe(featName);
+    window.location = location;
   });
-  // afterAll(() => {
-  //   process.env = OLD_ENV;
-  // });
 
-  it('should load / route', () => {
+  it('should return dfthe branch-name as part of the baseUrl when the ENVIRONMENT_NAME is uat', () => {
+    const featName = `/feat/uat-on-pr`;
+    const location = window.location;
+    delete window.location;
+    window.location = { pathname: `${featName}/` };
     process.env.ENVIRONMENT_NAME = 'uat';
-    route = '/';
-    pathname = '/feat/uat-on-pr/';
-    if (pathname.substring(pathname.length - route.length, pathname.length) === route) {
-      baseUrl = pathname.substring(0, pathname.length - route.length);
-    }
-    if (pathname.substring(pathname.length - route.length, pathname.length - 1) === `${route}/`) {
-      baseUrl = pathname.substring(0, pathname.length - route.length - 1);
-    }
-    // baseUrl = pathname.substring(0, pathname.length - route.length);
-    expect(baseUrl).toEqual('/feat/uat-on-pr');
-    expect(process.env.ENVIRONMENT_NAME).toEqual('uat');
-  });
-  it('should load /fea route', () => {
-    process.env.ENVIRONMENT_NAME = 'uat';
-    route = '/tracks/982536514732';
-    pathname = '/feat/uat-on-pr/tracks/982536514732';
-    if (pathname.substring(pathname.length - route.length, pathname.length) === route) {
-      baseUrl = pathname.substring(0, pathname.length - route.length);
-    }
-    if (pathname.substring(pathname.length - route.length, pathname.length - 1) === `${route}/`) {
-      baseUrl = pathname.substring(0, pathname.length - route.length - 1);
-    }
-    // baseUrl = pathname.substring(0, pathname.length - route.length - 1);
-    expect(baseUrl).toEqual('/feat/uat-on-pr');
-    expect(process.env.ENVIRONMENT_NAME).toEqual('uat');
+    const { getBaseUrl } = require('../history');
+
+    expect(getBaseUrl()).toBe(featName);
+    window.location = location;
   });
 });
